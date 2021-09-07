475158583a365665fc8b865ac37c25b302161e05
dvasdekis <dimitri@vasdekis.com.au>
2021-09-07 22:37:03 +0000

    Added files from learn_janus

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # learning_age
-Learning AGE encryption framework
+
+Learning Async with aiopg and encryption with age
+
+Let's collect some thoughts:
+
+Use case: Point at a directory and get a link to send to friends
+
+This needs:
+
+Server-side:
+* GUI to point at a directory (start with cmdline)
+* Scan & Encrypter to predict how many files/Usenet sets, and give us keys
+* Delta lake to write sets/keys sets to queue
+* Delta lake compressed with 7zip
+* Delta lake encrypted with age
+* Compress and encrypt and split into sets with 7zip
+* Par2 using AnimeTosho
+* Write out NZBs
+* Update Delta lake on Dat repo
+* Report secret string for NZB creation
+
+Client-side:
+1. Accept secret string
+2. Generate NZB
+3. Download with other tools
---
+++ main.py
@@ -0,0 +1,73 @@
+# Taken from https://pymotw.com/3/asyncio/executors.html
+
+import asyncio
+import concurrent.futures
+import logging
+import sys
+import time
+import os
+import random
+
+
+def this_func_blocks(n, print_log=False):
+    """
+    Ordinary python function that's executed procedurally
+    """
+    log = logging.getLogger('this_func_blocks({})'.format(n))
+    log.info('running')
+    time.sleep(0.1 * random.random() * n)
+    log.info('done')
+    result = n ** 2
+    if print_log == True:
+        print(f'{result}')
+    return result
+
+
+async def run_blocking_tasks(executor, blocking_function, num_tasks:int):
+    """
+    Async function that invokes run_in_executor to run a blocking function through multiple threads, and waits until they're all done
+
+    executor: Thread or Process pool that will run our tasks
+    blocking_function: Function object that will run on our process pool
+    num_tasks: How many tasks to queue up on our executor
+    """ 
+    log = logging.getLogger('run_blocking_tasks')
+    log.info('starting')
+
+    log.info('creating executor tasks')
+    loop = asyncio.get_event_loop()
+    blocking_tasks = [
+        loop.run_in_executor(executor, blocking_function, i)
+        for i in range(num_tasks)
+    ]
+    log.info('waiting for executor tasks')
+    completed, pending = await asyncio.wait(blocking_tasks)
+    results = [t.result() for t in completed]
+    log.info('results: {!r}'.format(results))
+
+    log.info('exiting')
+
+
+def main():
+    """
+    We want to do a POC where we pull a queue of tasks and payloads form SQLite, and process them asyncronously as they arrive
+    """
+    # Configure logging to show the id of the process where the log message originates.
+    logging.basicConfig(level=logging.INFO, format='PID %(process)5s %(name)18s: %(message)s', stream=sys.stderr)
+    
+    # Determine CPU core count
+    import multiprocessing
+    cpu_count = multiprocessing.cpu_count()
+    log = logging.getLogger('main')
+    log.info(f'CPUcount={cpu_count}')
+    # Use available cores to determine the number of simultaneous workers to run
+    thread_count = cpu_count * 2
+
+    # Create a process pool limited by max workers (thread count) using a context manager
+    with concurrent.futures.ProcessPoolExecutor(max_workers=thread_count) as executor:
+        # Use the current event loop to spawn a bunch of blocking tasks with our run_blocking_tasks function
+        asyncio.run(run_blocking_tasks(executor, blocking_function=this_func_blocks, num_tasks=thread_count*5, ))
+
+
+if __name__ == '__main__':
+    main()
